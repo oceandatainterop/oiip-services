25e4066941de146a9c7071f60d063ee0bfeb3703
Joe Roberts <joe.t.roberts@jpl.nasa.gov>
2018-05-10 18:28:10 -0700

    Added lat_err and lon_err to Tagbase geom materialized view

--- sql/create_mview_vis_geom.sql
+++ sql/create_mview_vis_geom.sql
@@ -7,11 +7,15 @@ TABLESPACE pg_default
 AS
  SELECT DISTINCT ON (vis_data.source_id, vis_data.position_date_time) vis_data.source_id,
     vis_data.position_date_time,
-    st_makepoint(vis_data.lon, vis_data.lat) AS geom
+    st_makepoint(vis_data.lon, vis_data.lat) AS geom,
+    vis_data.lat_err,
+    vis_data.lon_err
    FROM ( SELECT mview_vis_data.source_id,
             mview_vis_data.position_date_time,
             mview_vis_data.lon,
-            mview_vis_data.lat
+            mview_vis_data.lat,
+            mview_vis_data.lat_err,
+            mview_vis_data.lon_err
            FROM mview_vis_data) vis_data
   ORDER BY vis_data.source_id, vis_data.position_date_time
 WITH DATA;
