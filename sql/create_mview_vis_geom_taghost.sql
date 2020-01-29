23283f88f8d9a40f6c34768901506a4775cfc9cc
Joe Roberts <joe.t.roberts@jpl.nasa.gov>
2020-01-29 15:02:08 -0800

    Added taghost data source to GeoServer

--- sql/create_mview_vis_geom_taghost.sql
+++ sql/create_mview_vis_geom_taghost.sql
@@ -10,13 +10,13 @@ AS
     st_makepoint(vis_data.lon, vis_data.lat) AS geom,
     vis_data.lat_err,
     vis_data.lon_err
-   FROM ( SELECT mview_vis_data.source_id,
-            mview_vis_data.position_date_time,
-            mview_vis_data.lon,
-            mview_vis_data.lat,
-            mview_vis_data.lat_err,
-            mview_vis_data.lon_err
-           FROM mview_vis_data) vis_data
+   FROM ( SELECT mview_vis_data_tabular.source_id,
+            mview_vis_data_tabular.position_date_time,
+            mview_vis_data_tabular.lon,
+            mview_vis_data_tabular.lat,
+            mview_vis_data_tabular.lat_err,
+            mview_vis_data_tabular.lon_err
+           FROM mview_vis_data_tabular) vis_data
   ORDER BY vis_data.source_id, vis_data.position_date_time
 WITH DATA;
 
